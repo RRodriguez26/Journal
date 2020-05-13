7fce5ba1bc92aacb6893e8bba67573351946c75e
Rafael Perez <rafaelandresph@gmail.com>
2020-05-13 12:30:28 -0400

    Added a new method to create new window when user click "Write new journal."

--- Journal.py
+++ Journal.py
@@ -1,29 +1,31 @@
 from tkinter import *
+import tkinter as tk
 
-master = Tk()
+# Main Window
+master = tk.Tk()
 
-w = Canvas(master, width=1000, height=300)
+Canvas(master, width=1000, height=300).pack()
 
 pane = Frame(master)
 
-pane.pack(fill = BOTH,
-          expand = True)
+pane.pack(fill=NONE, expand=True)
 
-w.pack()
+button1 = Button(pane, text="Previously written", width=30).pack(fil=BOTH, expand=True)
 
-button1 = Button(pane,
-                 text = "Previously written",
-                 height = 20,
-                 width = 10)
 
-button1.pack(fill = BOTH,
-             expand = True)
+def newwindow():
 
-button2 = Button(pane,
-                 text = "Write new Journal")
+    toplevel = Toplevel()
+    toplevel.title('New Journal')
+    toplevel.geometry('1000x300')
 
-button2.pack(fill = BOTH,
-             expand = True)
+    label = tk.Label(toplevel, text="New Journal")
+    entry = tk.Entry(toplevel)
+    label.pack()
+    entry.pack()
+
+
+button2 = Button(pane, text="Write new Journal", command=newwindow).pack(fill=BOTH, expand=True)
+
 
 mainloop()
-
