f64f31e4dc496562eada7dd2e0a26af9df8bfd03
Rafael Perez <rafaelandresph@gmail.com>
2020-05-17 13:53:53 -0400

    Added comments and edited code

--- Journal.py
+++ Journal.py
@@ -2,30 +2,23 @@ from tkinter import *
 import tkinter as tk
 
 # Main Window
-master = tk.Tk()
+master = Tk()
 
-Canvas(master, width=1000, height=300).pack()
-
-pane = Frame(master)
-
-pane.pack(fill=NONE, expand=True)
-
-button1 = Button(pane, text="Previously written", width=30).pack(fil=BOTH, expand=True)
+Canvas(master, width=500, height=250).pack()
 
 
-def new_window():
+def new_window():  # Function called by button 2
 
-    top_level = Toplevel()
+    top_level = Tk()
     top_level.title('New Journal')
     top_level.geometry('1000x300')
 
-    label = tk.Label(top_level, text="New Journal")
-    entry = tk.Text(top_level, height=100, width=100)
-    label.pack()
+    entry = tk.Text(top_level, height=25, width=100)
     entry.pack()
 
-
-button2 = Button(pane, text="Write new Journal", command=new_window).pack(fill=BOTH, expand=True)
+# Two buttons to either write a new journal or a previous journal
+button1 = Button(text="Previously written", width=50).place(relx=0.5, rely=0.7, anchor=CENTER)
+button2 = Button(text="Write new Journal", command=new_window, width=50).place(relx=0.5, rely=0.5, anchor=CENTER)
 
 
 mainloop()
