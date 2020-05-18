a8f73fc940361f993c9871e05a891b6dcb8c599b
Rafael Perez <rafaelandresph@gmail.com>
2020-05-18 16:44:02 -0400

    Added more comments and added a method where the user can open a file, write on it, and close the file, it is not functional though.

--- Journal.py
+++ Journal.py
@@ -3,22 +3,32 @@ import tkinter as tk
 
 # Main Window
 master = Tk()
-
 Canvas(master, width=500, height=250).pack()
 
 
 def new_window():  # Function called by button 2
 
-    top_level = Tk()
-    top_level.title('New Journal')
-    top_level.geometry('1000x300')
+    # Creates new window
+    window = Tk()
+    window.title('New Journal')
+    window.geometry('1000x450')
 
-    entry = tk.Text(top_level, height=25, width=100)
+    # Allows for user to insert some text
+    entry = tk.Text(window, height=25, width=100)
     entry.pack()
+
+    # Opens a new file and users is allowed to write
+    text_entry = open("new_file.txt", 'w')
+    text_entry.write(entry.get('1.0', 'end-1c'))
+
+    # Save button for user to save written files
+    save = tk.Button(window, text="Save", width=10)
+    save.pack()
+    text_entry.close()
+
 
 # Two buttons to either write a new journal or a previous journal
 button1 = Button(text="Previously written", width=50).place(relx=0.5, rely=0.7, anchor=CENTER)
 button2 = Button(text="Write new Journal", command=new_window, width=50).place(relx=0.5, rely=0.5, anchor=CENTER)
-
 
 mainloop()
